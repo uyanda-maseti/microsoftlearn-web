d1dc91a3153fcb5608e147ccda760415aef9b018
Uyanda <128975995+uyanda-maseti@users.noreply.github.com>
2026-09-07 13:56:16 +0000

    create a shuffling array soluting

--- application.cs
+++ application.cs
@@ -1,5 +1,7 @@
-SayHello();
-DisplayRandomNumbers();
+// // SayHello();
+
+// Console.WriteLine("Generating random numbers:");
+// DisplayRandomNumbers();
 
 
 
@@ -7,17 +9,35 @@ DisplayRandomNumbers();
 
 
 
-void DisplayRandomNumbers()
+// void DisplayRandomNumbers()
+// {
+//     Random random = new Random();
+//     for (int i = 0; i < 5; i++)
+//     {
+//         Console.Write($"{random.Next(1, 100)} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void SayHello() 
+// {
+//     Console.WriteLine("Hello World!");
+// }
+
+Random random = new Random();
+
+int i = 0;
+int r = random.Next(pettingZoo.Length);
+
+string temp = pettingZoo[i];
+pettingZoo[i] = pettingZoo[r];
+pettingZoo[r] = temp;
+
+for (int i = 0; i < pettingZoo.Length; i++)
 {
-    Random random = new Random();
-    for (int i = 0; i < 5; i++)
-    {
-        Console.Write($"{random.Next(1, 100)} ");
-    }
-    Console.WriteLine();
-}
+    int r = random.Next(pettingZoo.Length);
 
-void SayHello() 
-{
-    Console.WriteLine("Hello World!");
+    string temp = pettingZoo[i];
+    pettingZoo[i] = pettingZoo[r];
+    pettingZoo[r] = temp;
 }
