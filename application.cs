2de829774ab71334426fafcc90573a095b311c00
Uyanda <128975995+uyanda-maseti@users.noreply.github.com>
2026-09-07 13:13:49 +0000

    finish up the method call and execute

--- application.cs
+++ application.cs
@@ -10,6 +10,11 @@ SayHello();
 void DisplayRandomNumbers()
 {
     Random random = new Random();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write($"{random.Next(1, 100)} ");
+    }
+    Console.WriteLine();
 }
 
 void SayHello() 
